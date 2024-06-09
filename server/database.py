9e3fc3f97e5bb4b3e8383b3aa2c016e3fd811274
Ashley Collinge <me@ashleycollinge.co.uk>
2024-06-09 01:35:17 +0100

    Removed old env file

--- server/database.py
+++ server/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy_utils import database_exists, create_database
 
-SQLALCHEMY_DATABASE_URL = "postgresql://jeremyadmin:Password!23@localhost:5432/db"
+SQLALCHEMY_DATABASE_URL = "postgresql://pybase:Password!23@192.168.1.224:5432/db"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 if not database_exists(engine.url):
